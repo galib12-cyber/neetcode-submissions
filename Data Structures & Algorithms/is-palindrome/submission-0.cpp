00b1ee94d6ab2719596cdcd8322c6e2dcd91068d
Shajidul Hoque Galib <shajidul.kafco@gmail.com>
2026-07-02 19:08:14 +0600

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        string t;
+        for (char c : s) {
+            if (isalnum(c)) {
+                t += tolower(c);
+            }
+        }
+         string r = t;
+        reverse(r.begin(),r.end());
+        
+        if (r == t)
+            return true;
+
+        else
+            return false;
+    }
+};
