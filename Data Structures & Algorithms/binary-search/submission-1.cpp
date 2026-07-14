baa6f567920d6739d422d4a07a355ca4b1e3db16
Shajidul Hoque Galib <shajidul.kafco@gmail.com>
2026-07-14 20:36:23 +0600

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int n = nums.size();
+        int r = n - 1;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+
+            if (nums[m] == target) return m;
+
+            if (target < nums[m])
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+
+        return -1;
+    }
+};
